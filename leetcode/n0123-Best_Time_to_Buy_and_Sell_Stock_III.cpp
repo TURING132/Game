7c459212955768a3cb4fe26117c54318f998a04e
Lee <llysglz@foxmail.com>
2025-03-10 21:20:51 +0800

    daily

--- leetcode/n0123-Best_Time_to_Buy_and_Sell_Stock_III.cpp
+++ leetcode/n0123-Best_Time_to_Buy_and_Sell_Stock_III.cpp
@@ -4,6 +4,17 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int n = prices.size();
+        vector<vector<int>> f(n, vector<int>(5));
+        f[0][1] = -prices[0];
+        f[0][3] = -prices[0]; // 关键一步，保证考虑了进行一次和进行两次交易的结果
+        for(int i = 1; i < prices.size(); i++){
+            f[i][0] = f[i-1][0];
+            f[i][1] = max(f[i-1][1], f[i-1][0] - prices[i]);
+            f[i][2] = max(f[i-1][2], f[i-1][1] + prices[i]);
+            f[i][3] = max(f[i-1][3], f[i-1][2] - prices[i]);
+            f[i][4] = max(f[i-1][4], f[i-1][3] + prices[i]);
+        }
+        return f[n-1][4];
     }
 };
--- leetcode/n0332-Reconstruct_Itinerary.cpp
+++ leetcode/n0332-Reconstruct_Itinerary.cpp
@@ -1,10 +1,35 @@
 #include <vector>
+#include <unordered_map>
+#include <map>
 #include <string>
 using namespace std;
 
 class Solution {
 public:
+    unordered_map<string, map<string, int>> f2;
+    int ticketsNum;
+    vector<string> res;
+    bool dfs(){
+        if(res.size()>ticketsNum) return true;
+        string cur = res.back();
+        for(pair<const string, int> & t : f2[cur]){
+            if(!t.second)continue;
+            t.second--;
+            res.push_back(t.first);
+            if(dfs()) return true;
+            t.second++;
+            res.pop_back();
+        }
+        return false;
+    }
+
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        
+        this->ticketsNum = tickets.size();
+        for(vector<string> & vec : tickets){
+            f2[vec[0]][vec[1]]++;
+        }
+        res.push_back("JFK");
+        dfs();
+        return res;
     }
 };
