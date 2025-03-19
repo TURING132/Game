f86b467b4965cdef4b1372e3a35ff99d2715282d
Lee <llysglz@foxmail.com>
2025-03-19 22:25:46 +0800

    daily

--- acwing/n0004-多重背包问题_I.cpp
+++ acwing/n0004-多重背包问题_I.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 const int N = 1010;
-int v[N], w[N], s[N], f[N][N];
+int s[N], v[N], w[N], f[N][N];
+int n,m;
 int main(){
-    int n, m;
     scanf("%d%d", &n, &m);
     for(int i = 1; i <= n; i++)scanf("%d%d%d", &v[i], &w[i], &s[i]);
-    for(int i = 1; i <= n; i++)
-        for(int j = 0; j <= m; j++)
-            for(int k = 0; k <= s[i] &&  k*v[i] <= j; k++){
-                f[i][j] = max(f[i][j], f[i-1][j - v[i]*k] + w[i]*k);
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j  <= m; j++){
+            for(int k = 0; k <= s[i] && j >= k * v[i]; k++){
+                f[i][j] = max(f[i-1][j - k * v[i]] + k * w[i], f[i][j]);
             }
-            
+        }
+    }
     printf("%d", f[n][m]);
-    
     return 0;
 }
